0e28cf801083606208254237ee929a8f14cfa8fe
Elbergg <tom.p.smolen@gmail.com>
2026-04-20 22:16:46 +0200

    started working on file

---
+++ src/file/file/src/file.rs
@@ -0,0 +1,25 @@
+use std::path::PathBuf;
+use std::fs;
+use filetime::FileTime;
+use std::io;
+
+pub mod file {
+    use super::*;
+
+    pub struct File<'a> {
+        pub path: PathBuf,
+        text: &'a str,
+        map: std::collections::HashMap<String, Vec<i32>>,
+    }
+
+    impl<'a> File<'a> {
+        pub fn new(path: PathBuf) -> File<'a> {
+
+        }
+        fn check_cache(&self) -> io::Result<bool>{
+            let read_content = fs::read_to_string(self.path)?;
+            let
+        }
+
+    }
+}
