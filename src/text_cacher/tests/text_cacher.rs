0a81c267987b7ee7191fa84cae2338f660dcd90f
Elbergg <tom.p.smolen@gmail.com>
2026-04-22 22:13:12 +0200

    fix the text_cacher tests

--- src/text_cacher/tests/text_cacher.rs
+++ src/text_cacher/tests/text_cacher.rs
@@ -4,8 +4,10 @@ use std::io::Read;
 use tempfile::tempdir;
 
 use crate::text_cacher::cache_text;
+use crate::file::file::TextFile;
 
-const DELIMITER: &str = "-----EOF-----";
+const DELIMITER: &str = "\x1E";
+
 #[test]
 fn test_cache_text_multiple_cases() {
     let test_cases = [
@@ -21,16 +23,20 @@ fn test_cache_text_multiple_cases() {
     for (text, expected_json_str) in test_cases {
         let dir = tempdir().expect("Failed to create temp dir");
         let file_path = dir.path().join("my_document.pdf");
-        let cache_path = cache_text(text, &file_path).expect("Failed to cache text");
+
+        let mut text_file_struct = TextFile::new_test(file_path.clone(), text.parse().unwrap()).expect("Failed to create TextFile");
+
+        let cache_path = cache_text(text, &file_path, &mut text_file_struct).expect("Failed to cache text");
         let mut file = File::open(cache_path).expect("Failed to open cache file");
         let mut a = String::new();
-        let content = file.read_to_string(&mut a);
-        assert!(content.is_ok());
+        file.read_to_string(&mut a).expect("Failed to read");
+
         let (json_str, text_str) = a.split_once(DELIMITER).expect("Delimiter not found");
         let parsed_map: HashMap<String, Vec<i32>> =
             serde_json::from_str(json_str).expect("Failed to parse map");
         let expected_map: HashMap<String, Vec<i32>> =
             serde_json::from_str(expected_json_str).expect("Failed to parse expected map");
+
         assert_eq!(parsed_map, expected_map, "Failed on input: {}", text);
         assert_eq!(text_str, text, "Text mismatch on input: {}", text);
     }
