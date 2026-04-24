05316fa6de36e50ee56fdf01b6228d107888387f
Elbergg <tom.p.smolen@gmail.com>
2026-04-24 19:11:18 +0200

    feat(cli): some changed in the cli

--- src/cli/search.py
+++ src/cli/search.py
@@ -1,11 +1,13 @@
 import click
 
+
 @click.command()
 @click.argument("file_name", type=click.Path())
-@click.option("-s", "--search", type=str, help="Phrase to search for in the file.")
-@click.option("-sm", "--semsearch", type=str, help="Phrase for semantic search.")
 @click.option("-r", "--reload", is_flag=True, help="Reload extracted text")
-def cli(file_name: str, search: str | None, semsearch: str | None, reload: bool):
+@click.option("-s", "--search", type=str, help="Phrase to search for in the file")
+@click.option("-sm", "--semsearch", type=str, help="Phrase for semantic search")
+@click.option("-si", "--interactive", is_flag=True, help="Interactive search mode")
+def cli(file_name: str, reload: bool, search: str | None, semsearch: str | None, interactive: bool):
     click.echo(f"File name: {file_name}")
     if reload:
         click.echo("Text extraction reloaded")
@@ -13,6 +15,9 @@ def cli(file_name: str, search: str | None, semsearch: str | None, reload: bool)
         click.echo(f"Search phrase: {search}")
     if semsearch:
         click.echo(f"Semantic search phrase: {semsearch}")
+    if interactive:
+        click.echo("Interactive search mode")
+
 
 if __name__ == "__main__":
     cli()
