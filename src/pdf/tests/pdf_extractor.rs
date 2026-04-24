d8b118115ec07de98ebd85c27640dd4bc594ac16
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-24 21:55:25 +0200

    test(pdf): add pdf extractor tests

---
+++ src/pdf/tests/pdf_extractor.rs
@@ -0,0 +1,41 @@
+use crate::pdf::{OcrEngine, PdfExtractor, Result};
+use image::DynamicImage;
+
+struct MockOcr;
+impl OcrEngine for MockOcr {
+    fn extract_text_from_image(&self, _data: DynamicImage) -> Result<String> {
+        Ok("mocked ocr text".to_string())
+    }
+}
+
+#[test]
+fn test_pdf_extraction_with_text() {
+    let path = format!(
+        "{}/src/pdf/tests/resources/text.pdf",
+        env!("CARGO_MANIFEST_DIR")
+    );
+    let mut extractor = PdfExtractor::open(&path).unwrap();
+    let ocr = MockOcr;
+    let text = extractor.extract_all_text(&ocr).unwrap();
+
+    assert!(text.contains("This is a test pdf"));
+}
+
+#[test]
+fn test_pdf_extraction_with_text_and_image() {
+    let path = format!(
+        "{}/src/pdf/tests/resources/text_and_image.pdf",
+        env!("CARGO_MANIFEST_DIR")
+    );
+    let mut extractor = PdfExtractor::open(&path).unwrap();
+    let ocr = MockOcr;
+    let text = extractor.extract_all_text(&ocr).unwrap();
+
+    assert!(text.contains("This is a test pdf with an image\nmocked ocr text\n"));
+}
+
+#[test]
+fn test_open_non_existent_file() {
+    let result = PdfExtractor::open("non_existent_file.pdf");
+    assert!(result.is_err());
+}
