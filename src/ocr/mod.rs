7dfd03ca1c3a18bf4f0c4c5bd16e3676c354a720
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-25 22:54:27 +0200

    feat(ocr): add utils for getting tessdata dir

--- src/ocr/mod.rs
+++ src/ocr/mod.rs
@@ -2,6 +2,8 @@ use image::DynamicImage;
 
 use crate::error::Result;
 
+mod utils;
+
 pub trait OcrEngine: Sync {
     fn extract_text_from_image(&self, image_data: DynamicImage) -> Result<String>;
 }
