5a6479cb9137e8882c00fc4bd847ea5d4bdeae6e
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-25 16:17:06 +0200

    chore: add `allow(unused)` directive temporarily

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+// TODO: remove later
+#![allow(unused)]
 use pyo3::prelude::*;
 
 mod constants;
 mod file;
-// TODO: get rid of pub later
 mod text_cacher;
 
 /// A Python module implemented in Rust.
