8216cc2be8e88535ff05387198476e91ec810cd7
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-27 00:15:19 +0200

    feat: implement basic pyfunction for `scan_search`

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,23 @@ mod ocr;
 mod text_cacher;
 mod text_extractor;
 
-/// A Python module implemented in Rust.
 #[pymodule]
 mod scan_search {
+    use std::path::{Path, PathBuf};
+
     use pyo3::prelude::*;
 
-    /// Formats the sum of two numbers as string.
+    use crate::{
+        error::ScanSearchError, file::TextFile, ocr::TesseractEngine, text_extractor::PdfExtractor,
+    };
+
+    /// Processes given pdf file and saves data to cache file and returns word map
     #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
+    fn process_file(path: String) -> PyResult<String> {
+        let ocr_engine = TesseractEngine::new("eng")?;
+        let text_extractor = PdfExtractor::new(&ocr_engine);
+        let file = TextFile::new(PathBuf::from(path), &text_extractor)?;
+        let word_map = serde_json::to_string(&*file.map).map_err(ScanSearchError::from)?;
+        Ok(word_map)
     }
 }
