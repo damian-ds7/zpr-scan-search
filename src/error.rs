e0976c801b6e8412687ac7293d8e299472f908bf
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-27 00:14:16 +0200

    feat(error): add `From<ScanSearchError> for PyErr` impl

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,30 @@
 use std::ffi::OsString;
 
+use pyo3::{PyErr, exceptions::PyRuntimeError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ScanSearchError {
-    #[error("Pdf error: {0}")]
+    #[error("{0}")]
     Pdf(#[from] pdf_oxide::error::Error),
 
-    #[error("IO error: {0}")]
+    #[error("{0}")]
     Io(#[from] std::io::Error),
 
-    #[error("OCR error: {0}")]
+    #[error("{0}")]
     Ocr(#[from] tesseract_rs::TesseractError),
 
-    #[error("JSON error: {0}")]
+    #[error("{0}")]
     Json(#[from] serde_json::Error),
 
     #[error("Path is not valid UTF-8: {0}")]
     InvalidPath(String),
+}
+
+impl From<ScanSearchError> for PyErr {
+    fn from(value: ScanSearchError) -> Self {
+        PyRuntimeError::new_err(value.to_string())
+    }
 }
 
 pub type Result<T> = std::result::Result<T, ScanSearchError>;
