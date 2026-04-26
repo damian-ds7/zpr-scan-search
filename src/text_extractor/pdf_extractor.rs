bb4482e89ce2b8f2c20cb52a577d1f488b8bfd1a
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-26 14:33:39 +0200

    refactor(text_extractor): modify to accept file path in extract method

--- src/text_extractor/pdf_extractor.rs
+++ src/text_extractor/pdf_extractor.rs
@@ -7,53 +7,42 @@ use std::path::Path;
 use crate::{error::Result, ocr::OcrEngine};
 
 pub struct PdfExtractor<'a, E: OcrEngine> {
-    document: PdfDocument,
     ocr_engine: &'a E,
 }
 
 impl<'a, E: OcrEngine> PdfExtractor<'a, E> {
-    pub fn open<P: AsRef<Path>>(path: P, ocr_engine: &'a E) -> Result<Self> {
-        let document = PdfDocument::open(path.as_ref().to_str().ok_or_else(|| {
+    pub fn new(ocr_engine: &'a E) -> Self {
+        Self { ocr_engine }
+    }
+}
+
+impl<'a, E: OcrEngine> TextExtractor for PdfExtractor<'a, E> {
+    fn extract_from(&self, path: &Path) -> Result<String> {
+        let mut document = PdfDocument::open(path.to_str().ok_or_else(|| {
             std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid path")
         })?)?;
-        Ok(Self {
-            document,
-            ocr_engine,
-        })
-    }
-
-    pub fn extract_all_text(&mut self, ocr_engine: &E) -> Result<String> {
         let mut full_text = String::new();
-        let page_count = self.document.page_count()?;
+        let page_count = document.page_count()?;
 
         for i in 0..page_count {
-            if let Ok(text) = self.document.extract_text(i) {
+            if let Ok(text) = document.extract_text(i) {
                 full_text.push_str(text.trim());
                 full_text.push('\n');
             }
 
-            let images = self.document.extract_images(i)?;
-
+            let images = document.extract_images(i)?;
             let ocr_results: Result<Vec<String>> = images
                 .into_par_iter()
                 .map(|img| {
                     let dynamic_image = img.to_dynamic_image()?;
-                    ocr_engine.extract_text_from_image(dynamic_image)
+                    self.ocr_engine.extract_text_from_image(dynamic_image)
                 })
                 .collect();
-
             for ocr_text in ocr_results? {
                 full_text.push_str(&ocr_text);
                 full_text.push('\n');
             }
         }
-
         Ok(full_text)
-    }
-}
-
-impl<'a, E: OcrEngine> TextExtractor for PdfExtractor<'a, E> {
-    fn extract(&mut self) -> Result<String> {
-        self.extract_all_text(self.ocr_engine)
     }
 }
