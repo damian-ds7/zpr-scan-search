6da8198b2df71bb3a35e8d2b57e8238f9c526bfd
Elbergg <tom.p.smolen@gmail.com>
2026-04-28 22:46:32 +0200

    chore: formatting and linting

--- python/scan_search/__init__.py
+++ python/scan_search/__init__.py
@@ -6,4 +6,4 @@ __doc__ = scan_search.__doc__  # noqa: F405
 if hasattr(scan_search, "__all__"):  # noqa: F405
     __all__ = scan_search.__all__  # noqa: F405
 
-atexit.register(flush_cache_writer)
+atexit.register(flush_cache_writer)  # noqa: F405
--- python/search.py
+++ python/search.py
@@ -1,5 +1,4 @@
 import click
-
 from scan_search import process_file
 
 
