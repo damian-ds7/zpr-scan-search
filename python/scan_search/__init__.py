67fa241e1c2d305e77785cdcd5d4202a2e088217
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-28 10:27:32 +0200

    feat: register flush_cache_writer with atexit

--- python/scan_search/__init__.py
+++ python/scan_search/__init__.py
@@ -1,5 +1,9 @@
+import atexit
+
 from .scan_search import *  # noqa: F403
 
 __doc__ = scan_search.__doc__  # noqa: F405
 if hasattr(scan_search, "__all__"):  # noqa: F405
     __all__ = scan_search.__all__  # noqa: F405
+
+atexit.register(flush_cache_writer)
