c7e4244605f773a422a08b82a1e42e02b641cf1e
Elbergg <tom.p.smolen@gmail.com>
2026-05-05 19:55:51 +0200

    feat(search): added loop improvments

--- src/text_searcher/mod.rs
+++ src/text_searcher/mod.rs
@@ -1,5 +1,4 @@
 use crate::file::TextFile;
-use rayon::iter::split;
 use std::collections::HashMap;
 
 #[cfg(test)]
@@ -7,23 +6,24 @@ pub mod tests;
 
 pub fn search(file: &TextFile, query: &String) -> Vec<i32> {
     let words: Vec<&str> = query.split_whitespace().collect();
-    let mut words_vec = vec![];
     let mut locations: Vec<i32> = vec![];
-    for &word in &words {
-        if let Some(value) = file.map.get(word) {
-            words_vec.push((word, value.clone()));
-        } else {
-            return locations;
-        }
-    }
-    words_vec.sort_by(|a, b| a.1.len().cmp(&b.1.len()));
-    let rarest = words_vec[0].clone();
+
+    let word_occur: Option<Vec<(&str, &Vec<i32>)>> = words
+        .clone()
+        .into_iter()
+        .map(|word| file.map.get(word).map(|occurances| (word, occurances)))
+        .collect();
+    let Some(mut valid_words) = word_occur else {
+        return locations;
+    };
+    valid_words.sort_by_key(|w| w.1.len());
+    let rarest = valid_words[0].clone();
     let index = words.iter().position(|&s| s == rarest.0).unwrap();
     let split_text: Vec<&str> = file.text.split_whitespace().collect(); // this is nasty, maybe we should consider the `File` object holding a split text
 
     for location in rarest.1 {
         let mut i: usize = 0;
-        let location_usize = location as usize;
+        let location_usize = *location as usize;
         for word in &words {
             if location_usize >= index {
                 if split_text[location_usize - index + i] != words[i] {
@@ -34,7 +34,7 @@ pub fn search(file: &TextFile, query: &String) -> Vec<i32> {
             }
             i += 1;
         }
-        locations.push(location);
+        locations.push(*location);
     }
     locations
 }
