647f4f2018e9090635964cc28ea677f4f7ff32f6
Elbergg <tom.p.smolen@gmail.com>
2026-05-16 15:03:34 +0200

    feat(semsearch) add two more tests plus rename nth to get_at in searcher

--- src/searcher/mod.rs
+++ src/searcher/mod.rs
@@ -2,7 +2,7 @@
 pub mod tests;
 
 pub trait SearchableIterator<'a> {
-    fn nth(&mut self, index: usize) -> Option<&'a str>;
+    fn get_at(&mut self, index: usize) -> Option<&'a str>;
 }
 pub trait Search {
     fn search(&self, query: &str) -> impl SearchableIterator<'_>;
