964ce91f98062de792bbec18d89475b4350fe11a
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-17 15:31:52 +0200

    refactor(extractor): update `ImageTextExtractor` to use `Arc` for `OcrEngine`

--- src/text_extractor/image_extractor.rs
+++ src/text_extractor/image_extractor.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, sync::Arc};
 
 use crate::{
     error::{Result, ScanSearchError},
@@ -9,18 +9,18 @@ use crate::{
 /// Extractor that uses an OCR engine to extract text from image files.
 #[allow(dead_code)] // TODO: remove allow
 pub struct ImageTextExtractor<E: OcrEngine> {
-    engine: E,
+    engine: Arc<E>,
 }
 
-impl<E: OcrEngine> ImageTextExtractor<E> {
+impl<E: OcrEngine + Sync + Send> ImageTextExtractor<E> {
     /// Creates a new ImageTextExtractor with the specified OCR engine.
     #[allow(dead_code)] // TODO: remove allow
-    pub fn new(engine: E) -> Self {
+    pub fn new(engine: Arc<E>) -> Self {
         Self { engine }
     }
 }
 
-impl<E: OcrEngine> TextExtractor for ImageTextExtractor<E> {
+impl<E: OcrEngine + Sync + Send> TextExtractor for ImageTextExtractor<E> {
     fn extract_from(&self, path: &Path) -> Result<String> {
         let image = image::open(path).map_err(|e| ScanSearchError::Image(e.to_string()))?;
         self.engine.extract_text_from_image(image)
