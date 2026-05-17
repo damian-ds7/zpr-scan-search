9d91723a6c8819cc95218092918fc4c8cd9bd6c1
Elbergg <tom.p.smolen@gmail.com>
2026-05-17 18:29:24 +0200

    refactor(search) add Embeddings struct

---
+++ src/text_cacher/embeddings.rs
@@ -0,0 +1,20 @@
+use std::ops::Deref;
+
+pub struct Embeddings(Vec<Vec<f32>>);
+
+impl Embeddings {
+    pub fn new() -> Self{
+        Embeddings(vec![])
+    }
+}
+
+impl Deref for Embeddings {
+    type Target = Vec<Vec<f32>>;
+    fn deref(&self) -> &Self::Target {&self.0}
+}
+
+impl From<Vec<Vec<f32>>> for Embeddings {
+    fn from(vec: Vec<Vec<f32>>) -> Self {
+        Embeddings(vec)
+    }
+}
