8802b71c02f446cbfdf86cf05022fe943ddfae55
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-17 22:14:59 +0200

    feat(lib): update python preview

--- python/search.py
+++ python/search.py
@@ -1,24 +1,24 @@
 import click
-from scan_search import process_file
+from scan_search import process_files
 
 
-def create_cache(file_name: str):
-    word_map = process_file(str(file_name))
+def create_cache(file_names: tuple[str, ...]):
+    word_map = process_files(*file_names)
     click.echo(word_map)
 
 
 @click.command()
-@click.argument("file_name", type=click.Path())
+@click.argument("file_names", type=click.Path(exists=True), nargs=-1)
 @click.option("-r", "--reload", is_flag=True, help="Reload extracted text")
 @click.option("-s", "--search", type=str, help="Phrase to search for in the file")
 @click.option("-sm", "--semsearch", type=str, help="Phrase for semantic search")
 @click.option("-si", "--interactive", is_flag=True, help="Interactive search mode")
-def cli(file_name: str, reload: bool, search: str | None, semsearch: str | None, interactive: bool):
+def cli(file_names: tuple[str, ...], reload: bool, search: str | None, semsearch: str | None, interactive: bool):
     if reload:
         click.echo("Text extraction reloaded")
 
     if not any([search, semsearch, interactive]):
-        create_cache(file_name)
+        create_cache(file_names)
         return
 
     if search:
