12898b07793489b4ddcaeddfe0cdcf6de006f24b
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-23 18:56:39 +0200

    refactor(encoder): use `Embeddings` wrapper in `TextEncoder` trait

--- src/text_encoder/fastembed.rs
+++ src/text_encoder/fastembed.rs
@@ -1,17 +1,18 @@
 use crate::error::Result;
 use crate::error::ScanSearchError;
+use crate::text_cacher::Embeddings;
 use crate::text_encoder::TextEncoder;
 use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
 /// Encoder using the fastembed lib
 pub struct FastEmbed;
 
 impl TextEncoder for FastEmbed {
-    fn encode(&self, text: &[&str]) -> Result<Vec<Vec<f32>>> {
+    fn encode(&self, text: &[&str]) -> Result<Embeddings> {
         let mut model = TextEmbedding::try_new(InitOptions::new(EmbeddingModel::AllMiniLML6V2))?;
 
         let embeddings: Vec<Vec<f32>> = model.embed(text, None)?;
 
-        Ok(embeddings)
+        Ok(Embeddings::from(embeddings))
     }
 }
 
