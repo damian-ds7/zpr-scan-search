ba12cc061c21951d997c31010a7ec4c8f601177b
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-24 20:24:09 +0200

    feat(cli): add `search` and `sem_search` methods for `Client`

--- src/cli/client.rs
+++ src/cli/client.rs
@@ -1,4 +1,9 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 use crate::{
     cli::utils::process_files,
@@ -6,9 +11,12 @@ use crate::{
     error::Result,
     file::{TextFile, TextFileLoader},
     ocr::TesseractEngine,
+    searcher::Search,
+    sem_searcher::SemSearcher,
     supported_file::InferDetector,
     text_encoder::fastembed::FastEmbed,
     text_extractor::UniversalExtractor,
+    text_searcher::TextSearcher,
 };
 
 pub struct Client {
@@ -33,4 +41,35 @@ impl Client {
             println!("{}", file.path().display());
         }
     }
+
+    pub fn search(&self, query: &str) -> Result<Vec<(PathBuf, Vec<Arc<str>>)>> {
+        self.files
+            .par_iter()
+            .map(|file| collect_search(TextSearcher::new(file.clone()), query, file.path()))
+            .collect()
+    }
+
+    pub fn sem_search(&self, query: &str) -> Result<Vec<(PathBuf, Vec<Arc<str>>)>> {
+        if !self.config.search_config.sem_search {
+            return Ok(vec![]);
+        }
+        self.files
+            .par_iter()
+            .map(|file| {
+                collect_search(
+                    SemSearcher::new(file.clone(), FastEmbed {}, 100),
+                    query,
+                    file.path(),
+                )
+            })
+            .collect()
+    }
+}
+
+fn collect_search<S: Search>(
+    searcher: S,
+    query: &str,
+    path: &Path,
+) -> Result<(PathBuf, Vec<Arc<str>>)> {
+    Ok((path.to_owned(), searcher.search(query)?.collect::<Vec<_>>()))
 }
