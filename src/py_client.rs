9b3e99b79bb47a3d699dc99eb670f187f7727c35
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-24 20:24:32 +0200

    feat(cli): update python facing client

--- src/py_client.rs
+++ src/py_client.rs
@@ -20,7 +20,6 @@ impl PyClient {
             .iter()
             .map(|p| p.extract::<PathBuf>())
             .collect::<PyResult<Vec<PathBuf>>>()?;
-
         let config = ScanSearchConfig::default();
         let inner = Client::new(config, paths)?;
         Ok(Self { inner })
@@ -28,5 +27,21 @@ impl PyClient {
 
     pub fn test(&self) {
         self.inner.test();
+    }
+
+    pub fn search(&self, query: &str) -> PyResult<Vec<(PathBuf, Vec<String>)>> {
+        let results = self.inner.search(query)?;
+        Ok(results
+            .into_iter()
+            .map(|(path, texts)| (path, texts.iter().map(|s| s.to_string()).collect()))
+            .collect())
+    }
+
+    pub fn sem_search(&self, query: &str) -> PyResult<Vec<(PathBuf, Vec<String>)>> {
+        let results = self.inner.sem_search(query)?;
+        Ok(results
+            .into_iter()
+            .map(|(path, texts)| (path, texts.iter().map(|s| s.to_string()).collect()))
+            .collect())
     }
 }
