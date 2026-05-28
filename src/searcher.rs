3a0f67020f4d84a9202171a0f65e34aace4d9408
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-28 18:05:26 +0200

    feat(searcher): add `ArcStrSlice` struct

--- src/searcher.rs
+++ src/searcher.rs
@@ -1,17 +1,52 @@
-use std::sync::Arc;
+use std::{ops::Range, sync::Arc};
 
 use crate::error::Result;
 
-#[derive(Debug, Default)]
-pub struct Context {
+#[derive(Debug, Default, Clone)]
+pub struct SearchContext {
     pub before: Option<usize>,
     pub after: Option<usize>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Query {
     pub term: String,
-    pub context: Context,
+    pub context: SearchContext,
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct ArcStrSlice {
+    text: Arc<str>,
+    range: Range<usize>,
+}
+
+impl ArcStrSlice {
+    pub fn new(text: Arc<str>, range: Range<usize>) -> Self {
+        Self { text, range }
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.text[self.range.clone()]
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct SearchResult {
+    pub before: ArcStrSlice,
+    pub matched: ArcStrSlice,
+    pub after: ArcStrSlice,
+}
+
+impl SearchResult {
+    pub fn before(&self) -> &str {
+        self.before.as_str()
+    }
+    pub fn matched(&self) -> &str {
+        self.matched.as_str()
+    }
+    pub fn after(&self) -> &str {
+        self.after.as_str()
+    }
 }
 
 /// Interface for structs implementing searching of some kind in the document
