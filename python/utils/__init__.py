bf161d7afe5d1694eaae118da20d296a99c305f8
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-30 17:58:12 +0200

    feat(config): add function to resolve config
    consolidates values given via cli flags, custom config path, default
    config path and default config if no config files exist

--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -1,3 +1,4 @@
+from .resolve_config import resolve_config
 from .tessdata_downloader import download_missing_traineddata
 
-__all__ = [download_missing_traineddata]
+__all__ = [download_missing_traineddata, resolve_config]
--- python/utils/resolve_config.py
+++ python/utils/resolve_config.py
@@ -1,17 +1,46 @@
 from pathlib import Path
 
+from classconf import ConfigParser
+from platformdirs import user_config_path
 from scan_search.config import ScanSearchConfig
 
 
 def resolve_config(
-    semsearch: str | None,
+    sem_search: bool,
     context_after: int | None,
     context_before: int | None,
     context: int | None,
-    follow_links: bool,
-    include_hidden: bool,
+    follow_links: bool | None,
+    include_hidden: bool | None,
     model: str | None,
     languages: tuple[str] | None,
     config_path: Path | None,
 ) -> ScanSearchConfig:
-    return ScanSearchConfig()
+    if config_path is None:
+        config_path = user_config_path("scan-search") / "config.toml"
+    if not config_path.exists():
+        config = ScanSearchConfig()
+    else:
+        parser = ConfigParser(config_path, ScanSearchConfig)
+        config = parser.get(ScanSearchConfig)
+
+    if follow_links:
+        config.fs_scan.follow_links = follow_links
+    if include_hidden:
+        config.fs_scan.include_hidden = include_hidden
+
+    resolved_before = context if context is not None else context_before
+    resolved_after = context if context is not None else context_after
+    if resolved_before is not None:
+        config.search.context_before = resolved_before
+    if resolved_after is not None:
+        config.search.context_after = resolved_after
+
+    config.search.sem_search = sem_search
+
+    if model is not None:
+        config.sem_search.model = model
+    if languages is not None:
+        config.ocr.languages = list(languages)
+
+    return config
